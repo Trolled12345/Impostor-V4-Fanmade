2f23d74c3c5feef98fc8988de6ea28d9089fb35a
Trolled12345 <95757284+Trolled12345@users.noreply.github.com>
2022-10-31 17:57:24 -0300

    Add files via upload

---
+++ assets/preload/stages/airship.lua
@@ -0,0 +1,97 @@
+local xx = 840;
+local yy = 625;
+local ofs = 30;
+local ofs2 = 30;
+local xx2 = 1250;
+local yy2 = 625;
+local followchars = true;
+
+function onCreate()
+		makeLuaSprite('sky', 'airship/airSky', -100, -0);
+		setScrollFactor('sky', 1, 1);
+		scaleObject('sky', 1, 1);
+
+		makeLuaSprite('yellow', 'airship/dead_yellow', -60, 810);
+		setScrollFactor('yellow', 1, 1);
+		scaleObject('yellow', 1.50, 1.50);
+		
+		addLuaSprite('sky', false);
+		 
+		if songName == "Oversight" or songName == "Influence" or songName == "Danger" or songName == "Death Blow" then
+			addLuaSprite('yellow', true);
+		end
+
+		doTweenX('CloudyTweenX', 'clouds', -1000, 1500);
+	end
+
+function onUpdate()
+    
+	if followchars == true then
+		
+        if mustHitSection == false then
+
+			if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            
+			if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            
+			if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+           
+			if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+		
+        else
+            
+			if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs2,yy2)
+            end
+            
+			if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs2,yy2)
+            end
+            
+			if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs2)
+            end
+           
+			if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs2)
+            end
+			
+			if getProperty('boyfriend.animation.curAnim.name') == 'singLEFTmiss' then
+                triggerEvent('Camera Follow Pos',xx2-ofs2,yy2)
+            end
+            
+			if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHTmiss' then
+                triggerEvent('Camera Follow Pos',xx2+ofs2,yy2)
+            end
+            
+			if getProperty('boyfriend.animation.curAnim.name') == 'singUPmiss' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs2)
+            end
+           
+			if getProperty('boyfriend.animation.curAnim.name') == 'singDOWNmiss' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs2)
+            end
+
+            if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+		
+        end
+    
+        triggerEvent('Camera Follow Pos','','')
+    
+	end
+    
+end
